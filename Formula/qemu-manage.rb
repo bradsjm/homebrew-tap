37b4fc140bdb315b603890cce5637f4b0c626420
user <user@macmini.lan>
2026-07-21 16:54:29 -0400

    qemu-manage 0.6.0

--- Formula/qemu-manage.rb
+++ Formula/qemu-manage.rb
@@ -1,9 +1,9 @@
 class QemuManage < Formula
   desc "Single-binary CLI for managing headless AArch64 QEMU VMs on Apple Silicon"
   homepage "https://github.com/bradsjm/qemu-manage"
-  url "https://github.com/bradsjm/qemu-manage/releases/download/v0.5.0/qemu-manage_0.5.0_darwin_arm64.tar.gz"
-  version "0.5.0"
-  sha256 "710910c14e2c84c1785a2cf8d78addc79257d66841bd0f4055c6636712c9e3cd"
+  url "https://github.com/bradsjm/qemu-manage/releases/download/v0.6.0/qemu-manage_0.6.0_darwin_arm64.tar.gz"
+  version "0.6.0"
+  sha256 "89b9ae09bcf7e46a922c389839c96329af2daa7d65aa5a2de392a9dad77a9986"
   license "Apache-2.0"
 
   depends_on arch: :arm64
@@ -17,6 +17,6 @@ class QemuManage < Formula
   test do
     output = shell_output("#{bin}/qemu-manage --version")
     assert_match "qemu-manage #{version}", output
-    assert_match "repository: #{homepage}", output
+    assert_match homepage, output
   end
 end
