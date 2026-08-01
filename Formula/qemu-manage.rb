a588c2564a0535a8aa10c3e08119b610f548f121
Jonathan Bradshaw <jb@nrgup.net>
2026-08-01 17:24:54 -0400

    Update qemu-manage to 0.7.0

--- Formula/qemu-manage.rb
+++ Formula/qemu-manage.rb
@@ -1,9 +1,8 @@
 class QemuManage < Formula
   desc "Single-binary CLI for managing headless AArch64 QEMU VMs on Apple Silicon"
   homepage "https://github.com/bradsjm/qemu-manage"
-  url "https://github.com/bradsjm/qemu-manage/releases/download/v0.6.1/qemu-manage_0.6.1_darwin_arm64.tar.gz"
-  version "0.6.1"
-  sha256 "7cd1c2652247aafa359b3436aebbc0a605e34eca49b2c4a52c1ab70aad645159"
+  url "https://github.com/bradsjm/qemu-manage/releases/download/v0.7.0/qemu-manage_0.7.0_darwin_arm64.tar.gz"
+  sha256 "04bb8cd823f8850828da253c316a7f3967711190b8d29d4208a2d10c27d98b86"
   license "Apache-2.0"
 
   depends_on arch: :arm64
